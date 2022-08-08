ef8cd26bc96af4268f6ec7f73b42cca0ff9d947b
akosszasz <gvnsaxon@gmail.com>
2022-08-08 18:54:32 +0200

    Sync git to bak

--- react_learning/my-app/src/components/UI/IconButton.js
+++
@@ -1,9 +0,0 @@
-import './IconButton.css';
-
-function IconButton(props) {
-    const classes = 'icon-button ' + props.className;
-    const clickEvent = props.onClick;
-    return <div className={classes} onClick={clickEvent}>{props.children}</div>;
-}
-
-export default IconButton;
